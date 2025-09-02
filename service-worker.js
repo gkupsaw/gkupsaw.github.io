e4cfc2240826ac2dd896337942d9afb51f55ed03
gkupsaw <griffin_kupsaw@brown.edu>
2025-09-02 14:54:05 -0400

    Deployment 2025-09-02

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.495a09097148bb6bda0490b1d887bd68.js"
+  "/precache-manifest.c0825cd2a60cdd19cda1eae4f39163f8.js"
 );
 
 self.addEventListener('message', (event) => {
